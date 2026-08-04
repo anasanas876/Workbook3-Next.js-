f000de6f8b2c9acbbdd2fbaf7bc11f6f531eac39
anasanas876 <okashaanas986@gmail.com>
2026-08-04 06:57:21 -0700

    Added Login Functionality

--- app/Login/page.js
+++ app/Login/page.js
@@ -1,9 +1,59 @@
-import Link from 'next/link'
-// Task 3
-function LoginPage(){
-    return <div>
-        <Link href="/dashboard">Go to Dashboard page</Link>
+"use client";
+
+import { useState } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
     
-    </div>
-}
-export default LoginPage
+      const response = await fetch("http://127.0.0.1:8000/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.status === 200) {
+        console.log("Login Successful");
+
+        const accessToken = data.access;
+        const refreshToken = data.refresh;
+
+        console.log(accessToken);
+        console.log(refreshToken);
+      }
+    } 
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+
+      <button type="submit">Login</button>
+    </form>
+  );
+
+
+export default Login;
